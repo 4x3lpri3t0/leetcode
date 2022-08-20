0fcda7a148a6800f5f5d6eebce6bec1213478671
Axel Prieto <prietoaxel@gmail.com>
2022-08-20 11:12:03 +0100

    Time: 184 ms (55.92%), Space: 14.1 MB (45.55%) - LeetHub

---
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.py
@@ -0,0 +1,7 @@
+# https://leetcode.com/problems/build-array-from-permutation/
+class Solution:
+    def buildArray(self, nums: list[int]) -> list[int]:
+        ans = [0] * len(nums)
+        for i in range(len(nums)):
+            ans[i] = nums[nums[i]]
+        return ans
