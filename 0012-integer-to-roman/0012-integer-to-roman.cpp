2fe1658bb218e98b85c29a893eb4c7abda1044cf
Axel Prieto <prietoaxel@gmail.com>
2023-04-20 10:18:59 +0100

    Time: 7 ms (77.38%), Space: 9.5 MB (12.00%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -1,19 +1,22 @@
-// https://leetcode.com/problems/integer-to-roman/
+// TC: O(1) since the algorithm always iterates through a constant number of values (13 in this case).
+// SC: O(1) since the amount of extra space used is constant (the size of the storeIntRoman vector, which is also 13 in this case
 class Solution
 {
 public:
     string intToRoman(int num)
     {
-        map<int, string> roman = {{1, "I"}, {4, "IV"}, {5, "V"}, {9, "IX"}, {10, "X"}, {40, "XL"}, {50, "L"}, {90, "XC"}, {100, "C"}, {400, "CD"}, {500, "D"}, {900, "CM"}, {1000, "M"}};
-        string result = "";
-        while (num > 0)
+        string Roman = "";
+        // Creating vector of pairs to store the Roman numeral values and their corresponding symbols
+        vector<pair<int, string>> storeIntRoman = {{1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"}, {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"}, {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}};
+        // Iterating through the vector and repeatedly adds the corresponding symbols to the result string while subtracting the corresponding value from the input integer until the input integer becomes zero.
+        for (int i = 0; i < storeIntRoman.size(); i++)
         {
-            auto it = roman.upper_bound(num);
-            it--;
-            result += it->second;
-            num -= it->first;
+            while (num >= storeIntRoman[i].first)
+            {
+                Roman += storeIntRoman[i].second;
+                num -= storeIntRoman[i].first;
+            }
         }
-
-        return result;
-    };
+        return Roman;
+    }
 };
